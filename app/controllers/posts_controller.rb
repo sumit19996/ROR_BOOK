939e725fe912d8eb5d1fa08010766accc32fc409
Sumit Sengar <sumits@shriffle.com>
2020-12-03 19:18:30 +0530

    added post feature

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+	def new
+		@post = Post.new
+	end
+	def create
+		@post = Post.new(post_params)
+		@post.user_id = current_user
+		if @post.save
+			redirect_to home_path
+		else
+			redirect_to new_post_path, { flash: "Post created Successfully"}
+		end
+	end
+	def show
+
+	end
+	private
+	def post_params
+		params.require(:post).permit(:image,:image_cache)
+	end
+end
