ab3f31b7b48b8be1f13cb8187d1f1dffed2ce9df
Sumit Sengar <sumits@shriffle.com>
2020-12-10 13:04:00 +0530

    changed the database to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_09_100005) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "likes", force: :cascade do |t|
     t.integer "account_id"
     t.integer "post_id"
